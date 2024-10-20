6e8bbf39168ccc8fc0f257b237a64e8620d9a8c7
Jakub Roman <jakub.m.roman@gmail.com>
2024-10-20 11:38:23 +0200

    Zaciatok section Work

--- src/pages/mainPage/components/work/Work.tsx
+++ src/pages/mainPage/components/work/Work.tsx
@@ -1,7 +1,22 @@
-
+import style from "./work.module.scss"
 
 export const Work = () => {
-    return (
-        <div></div>
-    )
-}
+  return (
+    <div className="w-[1400px] mx-auto my-[170px]">
+      {/* WORK TITLE/LINK BUTTON */}
+      <div
+      className="flex flex-col justify-center items-center"
+      >
+        <h1 className={`${style.title} text-center text-9xl font-extrabold`}>
+          Digital <br /> Creations
+        </h1>
+        <a className={`${style.linkButton} mt-[50px] text-xl font-normal rounded-[30px] px-[10px] py-[10px] text-[#ffffff]`}
+         href="#">
+          See More
+        </a>
+      </div>
+      {/* WORK BARS */}
+      <div></div>
+    </div>
+  );
+};
