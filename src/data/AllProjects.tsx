42d60c6a7d6ed0da30497b7ace6ad4d9a0d4667a
Jakub Roman <jakub.m.roman@gmail.com>
2025-03-12 20:05:02 +0100

    .

--- src/data/AllProjects.tsx
+++ src/data/AllProjects.tsx
@@ -1,7 +1,7 @@
 import reImg from "../assets/macbook-re4-work-img (1).png";
 import goProImg from "../assets/macbook-gopro-work-img.png";
-import artGalleryImg from "../assets/macbook-art-gallery-festival-img.png"
-import moneySaverImg from "../assets/money-saver-img..png"
+import artGalleryImg from "../assets/macbook-art-gallery-festival-img.png";
+import moneySaverImg from "../assets/money-saver-img..png";
 import { useTranslation } from "react-i18next";
 
 type Project = {
@@ -13,40 +13,38 @@ type Project = {
 };
 
 export const AllProjects = () => {
-
-  const {t} = useTranslation()
+  const { t } = useTranslation();
 
   const project: Project[] = [
     {
       id: "goPro-shop-work",
-      title: t('work.workBars.goProShoppingPage.title'),
-      desc: t('work.workBars.goProShoppingPage.desc'),
+      title: t("work.workBars.goProShoppingPage.title"),
+      desc: t("work.workBars.goProShoppingPage.desc"),
       image: goProImg,
       link: "https://kubanec01.github.io/ActionCam-Store/",
     },
     {
       id: "money-saver-app",
-      title: t('work.workBars.moneySaverApp.title'),
-      desc: t('work.workBars.moneySaverApp.desc'),
+      title: t("work.workBars.moneySaverApp.title"),
+      desc: t("work.workBars.moneySaverApp.desc"),
       image: moneySaverImg,
-      link: "https://github.com/Kubanec01/Money_Saver_App/",
+      link: "https://kubanec01.github.io/Money_Saver_App/",
     },
     {
       id: "resident-evil-work",
-      title: t('work.workBars.residentEvil4.title'),
-      desc: t('work.workBars.residentEvil4.desc'),
+      title: t("work.workBars.residentEvil4.title"),
+      desc: t("work.workBars.residentEvil4.desc"),
       image: reImg,
       link: "https://kubanec01.github.io/ResidentEvil4Wiki/",
     },
     {
       id: "art-gallery-festival",
-      title: t('work.workBars.artGalleryFestival.title'),
-      desc: t('work.workBars.artGalleryFestival.desc'),
+      title: t("work.workBars.artGalleryFestival.title"),
+      desc: t("work.workBars.artGalleryFestival.desc"),
       image: artGalleryImg,
       link: "https://kubanec01.github.io/Art_Gallery_Festival/",
     },
   ];
 
-  return project
-}
-
+  return project;
+};
