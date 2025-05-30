a53ee74bdbafcb1b9f651ae7dc917498de97f5de
Jakub Roman <jakub.m.roman@gmail.com>
2025-05-30 23:35:02 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import {BrowserRouter  as Router } from "react-router-dom"
-import { MainRoutes } from "./routes/MainRoutes"
-import { NavBar } from "./components/navbar/Navbar"
-import { Footer } from "./components/footer/Footer"
-import { ScrollButton } from "./components/scrollButton/ScrollButton"
+import { MainRoutes } from "./routes/MainRoutes";
+import { NavBar } from "./components/navbar/Navbar";
+import { Footer } from "./components/footer/Footer";
+import { ScrollButton } from "./components/scrollButton/ScrollButton";
 
 function App() {
   return (
-    <Router 
-    basename="/Portfolio"
-    >
+    <>
       <NavBar />
       <MainRoutes />
       <Footer />
       <ScrollButton />
-    </Router>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
